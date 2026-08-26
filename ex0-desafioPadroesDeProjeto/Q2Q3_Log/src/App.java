5342d9bd901c831834b405808ed1cb3dc8e448ff
Boeira22 <eduardoboeira@outlook.com>
2026-08-26 18:46:01 -0300

    Questao_2_3_Lucas

--- ex0-desafioPadroesDeProjeto/Q2Q3_Log/src/App.java
+++ ex0-desafioPadroesDeProjeto/Q2Q3_Log/src/App.java
@@ -1,11 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        LogSimples log = new LogSimples();
+
+        LogSimples log = LogSimples.getInstance();
+
         log.log("Mensagem de alerta 1");
         log.log("Mensagem de alarme 23");
         log.log("Mensagem de PANICO!!");
 
-        for(String m:log){
+        for (String m : log) {
             System.out.println(m);
         }
     }
--- ex0-desafioPadroesDeProjeto/Q2Q3_Log/src/LogSimples.java
+++ ex0-desafioPadroesDeProjeto/Q2Q3_Log/src/LogSimples.java
@@ -3,14 +3,25 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-public class LogSimples implements Iterable<String>{
+public class LogSimples implements Iterable<String> {
+
     private List<String> mensagens;
 
-    public LogSimples(){
+    private static LogSimples instancia;
+
+    private LogSimples() {
         mensagens = new LinkedList<>();
     }
 
-    public void log(String m){
+    public static LogSimples getInstance() {
+        if (instancia == null) {
+            instancia = new LogSimples();
+        }
+
+        return instancia;
+    }
+
+    public void log(String m) {
         String logM = LocalDate.now().toString() + " : " + m;
         mensagens.add(logM);
     }
